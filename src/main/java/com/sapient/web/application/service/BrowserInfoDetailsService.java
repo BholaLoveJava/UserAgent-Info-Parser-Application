61c2751ff184a3668cd68d97ed54e0f123ec92e6
Bhola Kumar <bhola.kumar1990@gmail.com>
2021-09-12 20:14:11 +0530

    YetAnotherAnalyzer api code added
    YetAnotherAnalyzer api code added

--- src/main/java/com/sapient/web/application/service/BrowserInfoDetailsService.java
+++ src/main/java/com/sapient/web/application/service/BrowserInfoDetailsService.java
@@ -8,5 +8,7 @@ public interface BrowserInfoDetailsService {
 	public BrowserDeviceOSInfoResponse getBrowserInfoDetails(UserAgentRequest userAgentRequest);
 
 	public BrowserDeviceOSInfoResponse getBrowserInfoDetailsV2(UserAgentRequest userAgentRequest);
+	
+	public BrowserDeviceOSInfoResponse getBrowserInfoDetailsV3(UserAgentRequest userAgentRequest);
 
 }
